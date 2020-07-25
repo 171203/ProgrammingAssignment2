317c6516874dd8a5f756cdd1e34e40845531e8da
171203 <61779254+171203@users.noreply.github.com>
2020-07-25 12:47:52 +0530

    Lexical.R

--- Lexical scoping.R
+++ Lexical scoping.R
@@ -1,33 +1,41 @@
-create_cache_matrix<-function(x=matrix())
+##Written by : Mohan 
+## Data      :  25th july 2020
+## Finding the inverse of the cache matrix 
+
+## This function will create the special matrix object which will support cache matrix for inverse
+create_cache_matrix<-function(x=matrix()) ## using the function matrix as a argument which is available in R
+   
    {
-     inv<-NULL
-     set<-function(m)
-     {
-       x<<-m
-       inv<<-NULL
+     j<-NULL           ##j will take value of matrix inverse
+     set<-function(m)    ## Defining the set function to assign new value of matrix in parent environment
+     {                   
+       x<<-m            
+       j<<-NULL       ##If there is a new matrix then reset j to NULL
+      
         }
       get<-function()x
-      setInverse<-function(inverse)inv<<-inverse
-      getInverse<-function()inv
-      list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+      setInverse<-function(inverse)j<<-inverse    ##It assigns the value of j in parent environment
+      getInverse<-function()j                  ## It gives the value of j
+      list(set=set,get=get,setInverse=setInverse,getInverse=getInverse) ##It indicates the order in which it takes
       }
+
+##This function will computes the inverse of the matrix returned by the create_cache_matrix
+##If the inverse of the matrix already computed then cache_solution will restore that inverse from cache
+
   cache_solution<-function(x,...)
     {
-      inv<-x$getInverse()
-      if(!is.null(inv))
+      j<-x$getInverse()
+      if(!is.null(j))
       {
        message("Given data is getting ready for the catched data")
-       return(inv)
+       return(j)
        }
 
    matr<-x$get()
-   inv<-solve(mat,...)
-   x$setInverse(inv)
-    inv
+   j<-solve(mat,...)
+   x$setInverse(j)
+    j
     }
-    my_matrix<-create_cache_matrix(matrix(1:4,2,2))
-    my_matrix$get()
-    my_matrix$getInverse()
-    cache_solution(my_matrix)
+    
     
 
